1d6471a883f716ae5b7bf2d579fa4972cc57a8a9
dlgksqls <horainlee87@gmail.com>
2024-01-05 15:37:58 +0900

    포스트 crud 구현

--- blog/post/views.py
+++ blog/post/views.py
@@ -10,13 +10,31 @@ from .serializers import PostModelSerializers, CommentModelSerializers
 
 # Create your views here.
 
-class PostListView(generics.ListAPIView):
-  queryset = Post.objects.all()
-  serializer_class = PostModelSerializers
 
-@api_view(["GET"])
+class PostListView(generics.ListAPIView):
+    queryset = Post.objects.all()
+    serializer_class = PostModelSerializers
+
+
+@api_view(["GET", "PATCH", "DELETE"])
 def PostDetailView(request, id):
-  post = get_object_or_404(Post, id = id)
-  serializer = PostModelSerializers(post)
-  return Response(serializer.data, status=status.HTTP_200_OK)
-  
+    post = get_object_or_404(Post, id=id)
+
+    if request.method == "GET":
+        serializer = PostModelSerializers(post)
+        return Response(serializer.data, status=status.HTTP_200_OK)
+    elif request.method == "PATCH":
+        serializer = PostModelSerializers(post, data=request.data, partial=True)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=status.HTTP_202_ACCEPTED)
+        else:
+            return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+    elif request.method == "DELETE":
+        post.delete()
+        return Response({"detail": "Delete Complete"}, status=status.HTTP_202_ACCEPTED)
+    else:
+        return Response(
+            {"detail": "Invalid request method"},
+            status=status.HTTP_405_METHOD_NOT_ALLOWED,
+        )
